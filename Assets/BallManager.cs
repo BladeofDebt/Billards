752758fe8551d1b8fadc92c64af7bba5af4891e2
River Govers <river.govers@ad.aie.edu>
2016-09-01 14:29:00 +1000

    Pushing the ball manager. Contains Balls property and RemoveBall.
    RemoveBall must be called before destorying the gameobject.

---
+++ Assets/BallManager.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class BallManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject[] m_gameobjectarrayBalls = null;
+
+    // Created: River
+    // Date: 9/1/2016
+    // Time: 2:26
+    /// <summary>
+    /// Returns a list of all the balls in the ball list
+    /// </summary>
+    public GameObject[] Balls
+    {
+        get
+        {
+            return m_gameobjectarrayBalls;
+        }
+    }
+
+    // Created: River
+    // Date: 9/1/2016
+    // Time: 1:58
+    void EndGame()
+    {
+
+    }
+
+    // Created: River
+    // Date: 9/1/2016
+    // Time: 2:04
+    /// <summary>
+    /// Removes the ball from the registered ball list
+    /// </summary>
+    /// <param name="a_gameobjectBall"></param>
+    public void RemoveBall(GameObject a_gameobjectBall)
+    {
+        bool boolFound = false;
+
+        for (int i = 0; i < m_gameobjectarrayBalls.Length; i++)
+        {
+            if (a_gameobjectBall == m_gameobjectarrayBalls[i])
+            {
+                m_gameobjectarrayBalls[i] = null;
+                boolFound = true;
+                break;
+            }
+        }
+
+        if (!boolFound)
+        {
+            return;
+        }
+
+        GameObject[] gameobjectarrayTemp = new GameObject[m_gameobjectarrayBalls.Length - 1];
+
+        int j = 0;
+        for (int i = 0; i < m_gameobjectarrayBalls.Length; ++i)
+        {
+            if (m_gameobjectarrayBalls[j] == null)
+            {
+                ++j;
+            }
+
+            gameobjectarrayTemp[i] = m_gameobjectarrayBalls[j];
+
+            ++j;
+        }
+
+        m_gameobjectarrayBalls = gameobjectarrayTemp;
+
+        System.GC.Collect(0);
+    }
+
+    // Update is called once per frame
+    // Created: River
+    // Date: 9/1/2016
+    // Time: 1:56
+    void Update()
+    {
+        if (m_gameobjectarrayBalls.Length == 0)
+        {
+            EndGame();
+        }
+        else
+        {
+            // TODO: Remove
+            RemoveBall(m_gameobjectarrayBalls[0]);
+        }
+    }
+}
