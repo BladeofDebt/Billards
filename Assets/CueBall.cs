cbcb59c537687d8c9eaa8a62372d6407eb7fa536
River Govers <river.govers@ad.aie.edu>
2016-09-01 15:29:17 +1000

    Real billards push

--- Assets/CueBall.cs
+++ Assets/CueBall.cs
@@ -4,15 +4,18 @@ using System.Collections;
 public class CueBall : MonoBehaviour
 {
     private Vector3 m_position;
-    private Rigidbody2D m_rigidbody;
+    private Rigidbody2D m_rigidBody;
     private Vector3 m_force;
+    private Vector3[] m_vertices = new Vector3[2];
+    private LineRenderer m_lineRenderer;
 
     // Use this for initialization
     void Start()
     {
 
         //m_position = new Vector3(5, 0, 0);
-        m_rigidbody = this.GetComponent<Rigidbody2D>();
+        m_rigidBody = this.GetComponent<Rigidbody2D>();
+        m_lineRenderer = this.GetComponent<LineRenderer>();
     }
 
     // Update is called once per frame
@@ -23,19 +26,25 @@ public class CueBall : MonoBehaviour
         if (Input.GetMouseButton(0))
         {
 
-            m_force = m_rigidbody.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            m_force = m_rigidBody.transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
             m_force.z = 0;
             Ray a = new Ray(transform.position, Camera.main.ScreenToWorldPoint(Input.mousePosition));
             Debug.DrawLine(a.origin, Camera.main.ScreenToWorldPoint(Input.mousePosition));
-
+            m_vertices[0] = transform.position;
+            m_vertices[1] = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
         else
         if (Input.GetMouseButtonUp(0))
         {
-            m_rigidbody.AddForce(m_force * mouseDist.magnitude * 50 * Time.deltaTime, ForceMode2D.Impulse);
-
+            m_rigidBody.AddForce(m_force * mouseDist.magnitude * 50 * Time.deltaTime, ForceMode2D.Impulse);
+            m_vertices[0] = Vector3.zero;
+            m_vertices[1] = Vector3.zero;
         }
-        m_rigidbody.velocity *= 0.995f;//friction decay
+        m_vertices[0].z = 0;
+        m_vertices[1].z = 0;
+        m_lineRenderer.SetPositions(m_vertices);
+
+        m_rigidBody.velocity *= 0.995f;//friction decay
     }
 }
